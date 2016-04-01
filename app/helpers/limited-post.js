d6377a6b39ed943de6ddd10220aed042eb47aa58
eddie duro and josh overly <josh@>
2016-03-31 17:35:10 -0700

    fix minor bugs

--- app/helpers/limited-post.js
+++ app/helpers/limited-post.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 
-export function limitedPost(params) {
+export function limitedPost(params, hash) {
+  console.log()
   var count = 0;
   var postArray = [];
   for(var i = 0; i < 6; i++){
     count = count + 1;
     postArray.push(params[0].content[i]);
   }
-  console.log(postArray.slice(0,6));
-  return postArray.slice(0,6);
+  hash = {postArray.slice(0,6)};
+  console.log(hash);
   // if(params.length < 5){
   //   console.log('yo');
   // }
